79e4f0f197e6b3ba600c515285c719bed4a98964
Kris <ksc25730241@gmail.com>
2026-02-16 11:26:50 +0800

    Add new lib and function: itertools &  naked_triple_line_and_box

--- Sudoku.py
+++ Sudoku.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from itertools import combinations
 
 
 class SudokuGrid:
@@ -85,7 +86,7 @@ class SudokuGrid:
                     x_list = [pos[0] for pos in positions]
                     y_list = [pos[1] for pos in positions]
 
-                    # If all candidates are in the same row, eliminate that candidate from the rest of the row
+                    # If all candidates are in the same row, eliminate that candidate from the rest of the row and box
                     if len(set(x_list)) == 1:
                         row = x_list[0]
                         for col in range(9):
@@ -104,6 +105,63 @@ class SudokuGrid:
                         col = y_list[0]
                         for row in range(9):
                             if row < box_row * 3 or row >= (box_row + 1) * 3:
+                                if (
+                                    isinstance(self.candidates[row, col], set)
+                                    and candidate in self.candidates[row, col]
+                                ):
+                                    self.candidates[row, col].discard(candidate)
+                                    self.vars["any_changes"] = True
+                                    if not self.candidates[row, col]:
+                                        return False  # Invalid puzzle state
+
+        # For each row, check if there is a candidate that only appears in one box, delete that candidate from the rest of the box
+        for i in range(9):
+            row_candidates = {}
+            for j in range(9):
+                if isinstance(self.candidates[i, j], set):
+                    for candidate in self.candidates[i, j]:
+                        if candidate not in row_candidates:
+                            row_candidates[candidate] = []
+                        row_candidates[candidate].append(j)
+
+            for candidate, positions in row_candidates.items():
+                box_col_start = (positions[0] // 3) * 3
+                box_row_start = (i // 3) * 3
+
+                if all(
+                    (pos // 3) * 3 == box_col_start for pos in positions
+                ):  # All candidates are in the same box
+                    for col in range(box_col_start, box_col_start + 3):
+                        for row in range(box_row_start, box_row_start + 3):
+                            if row != i:
+                                if (
+                                    isinstance(self.candidates[row, col], set)
+                                    and candidate in self.candidates[row, col]
+                                ):
+                                    self.candidates[row, col].discard(candidate)
+                                    self.vars["any_changes"] = True
+                                    if not self.candidates[row, col]:
+                                        return False  # Invalid puzzle state
+
+        # For each column, check if there is a candidate that only appears in one box, delete that candidate from the rest of the box
+        for j in range(9):
+            col_candidates = {}
+            for i in range(9):
+                if isinstance(self.candidates[i, j], set):
+                    for candidate in self.candidates[i, j]:
+                        if candidate not in col_candidates:
+                            col_candidates[candidate] = []
+                        col_candidates[candidate].append(i)
+
+            for candidate, positions in col_candidates.items():
+                box_row_start = (positions[0] // 3) * 3
+                box_col_start = (j // 3) * 3
+                if all(
+                    (pos // 3) * 3 == box_row_start for pos in positions
+                ):  # All candidates are in the same box
+                    for row in range(box_row_start, box_row_start + 3):
+                        for col in range(box_col_start, box_col_start + 3):
+                            if col != j:
                                 if (
                                     isinstance(self.candidates[row, col], set)
                                     and candidate in self.candidates[row, col]
@@ -137,7 +195,8 @@ class SudokuGrid:
 
         self.updating_candidates()
 
-    def naked_candidate_line(self):
+    def naked_candidate_pair_line_and_box(self):
+        ### Note that this is working on for candidates pair.
         # For each row, there is a naked pair or naked triple, if there are 2 or 3 cells in the same row that have the same 2 or 3 candidates, then those candidates can be eliminated from the rest of the row
         for i in range(9):
             row_candidates = {}
@@ -184,7 +243,122 @@ class SudokuGrid:
                                 if not self.candidates[row, j]:
                                     return False  # Invalid puzzle state
 
+        # For each box, there is a naked pair or naked triple, if there are 2 or 3 cells in the same row that have the same 2 or 3 candidates, then those candidates can be eliminated from the rest of the row
+        for box_row in range(3):
+            for box_col in range(3):
+                box_candidates = {}
+                for i in range(box_row * 3, (box_row + 1) * 3):
+                    for j in range(box_col * 3, (box_col + 1) * 3):
+                        if isinstance(self.candidates[i, j], set):
+                            candidates_tuple = tuple(sorted(self.candidates[i, j]))
+                            if candidates_tuple not in box_candidates:
+                                box_candidates[candidates_tuple] = []
+                            box_candidates[candidates_tuple].append((i, j))
+
+                for candidates_tuple, positions in box_candidates.items():
+                    if len(candidates_tuple) == len(positions) and len(positions) > 1:
+                        for i in range(box_row * 3, (box_row + 1) * 3):
+                            for j in range(box_col * 3, (box_col + 1) * 3):
+                                if (i, j) not in positions:
+                                    if isinstance(self.candidates[i, j], set) and any(
+                                        candidate in self.candidates[i, j]
+                                        for candidate in candidates_tuple
+                                    ):
+                                        self.candidates[i, j] -= set(candidates_tuple)
+                                        self.vars["any_changes"] = True
+                                        if not self.candidates[i, j]:
+                                            return False  # Invalid puzzle state
+
         self.updating_candidates()
+
+    def naked_triple_line_and_box(self):
+        # For each box
+        for box_row in range(3):
+            for box_col in range(3):
+                cells_candidates = {}
+                # Collect all cells with 2 or 3 candidates in the box
+                for i in range(box_row * 3, (box_row + 1) * 3):
+                    for j in range(box_col * 3, (box_col + 1) * 3):
+                        if (
+                            isinstance(self.candidates[i, j], set)
+                            and 2 <= len(self.candidates[i, j]) <= 3
+                        ):
+                            cells_candidates[(i, j)] = self.candidates[i, j]
+
+                # Check all possible combinations of 3 cells
+                for cells in combinations(cells_candidates.items(), 3):
+                    positions = [cell[0] for cell in cells]
+                    candidates_sets = [cell[1] for cell in cells]
+                    union_candidates = set().union(*candidates_sets)
+
+                    # If union has 3 numbers and all cells only contain these numbers
+                    if len(union_candidates) == 3:
+                        # Remove these candidates from other cells in the box
+                        for i in range(box_row * 3, (box_row + 1) * 3):
+                            for j in range(box_col * 3, (box_col + 1) * 3):
+                                if (i, j) not in positions:
+                                    if isinstance(self.candidates[i, j], set):
+                                        before_len = len(self.candidates[i, j])
+                                        self.candidates[i, j] -= union_candidates
+                                        if len(self.candidates[i, j]) != before_len:
+                                            self.vars["any_changes"] = True
+                                        if not self.candidates[i, j]:
+                                            return False  # Invalid puzzle state
+
+        # For each row
+        for i in range(9):
+            cells_candidates = {}
+            for j in range(9):
+                if (
+                    isinstance(self.candidates[i, j], set)
+                    and 2 <= len(self.candidates[i, j]) <= 3
+                ):
+                    cells_candidates[j] = self.candidates[i, j]
+
+            for cols in combinations(cells_candidates.items(), 3):
+                positions = [col[0] for col in cols]
+                candidates_sets = [col[1] for col in cols]
+                union_candidates = set().union(*candidates_sets)
+
+                if len(union_candidates) == 3:
+                    for j in range(9):
+                        if j not in positions:
+                            if isinstance(self.candidates[i, j], set):
+                                before_len = len(self.candidates[i, j])
+                                self.candidates[i, j] -= union_candidates
+                                if len(self.candidates[i, j]) != before_len:
+                                    self.vars["any_changes"] = True
+                                if not self.candidates[i, j]:
+                                    return False
+
+        # For each column
+        for j in range(9):
+            cells_candidates = {}
+            for i in range(9):
+                if (
+                    isinstance(self.candidates[i, j], set)
+                    and 2 <= len(self.candidates[i, j]) <= 3
+                ):
+                    cells_candidates[i] = self.candidates[i, j]
+
+            for rows in combinations(cells_candidates.items(), 3):
+                positions = [row[0] for row in rows]
+                candidates_sets = [row[1] for row in rows]
+                union_candidates = set().union(*candidates_sets)
+
+                if len(union_candidates) == 3:
+                    for i in range(9):
+                        if i not in positions:
+                            if isinstance(self.candidates[i, j], set):
+                                before_len = len(self.candidates[i, j])
+                                self.candidates[i, j] -= union_candidates
+                                if len(self.candidates[i, j]) != before_len:
+                                    self.vars["any_changes"] = True
+                                if not self.candidates[i, j]:
+                                    return False
+
+        self.updating_candidates()
+        return True
 
     def fill_in_single_candidate(self):
         ### in terms of candidates choice
@@ -294,13 +468,15 @@ class SudokuGrid:
 
         self.only_candidate_in_box()
         self.hidden_candidate_line()
-        self.naked_candidate_line()
+        self.naked_candidate_pair_line_and_box()
+        self.naked_triple_line_and_box()
         while self.check_if_single_candidate() or self.vars["any_changes"]:
             self.vars["any_changes"] = False
             self.fill_in_single_candidate()
             self.only_candidate_in_box()
             self.hidden_candidate_line()
-            self.naked_candidate_line()
+            self.naked_candidate_pair_line_and_box()
+            self.naked_triple_line_and_box()
 
 
 if __name__ == "__main__":
@@ -311,7 +487,9 @@ if __name__ == "__main__":
 
     s = SudokuGrid(data)
     s.solve()
+    print(s.grid)
+    print(s.candidates)
 
     # export the result to a csv file
-    df = pd.DataFrame(s.grid)
-    df.to_csv("output_sudoku.csv", index=False, header=False)
+    # df = pd.DataFrame(s.grid)
+    # df.to_csv("output_sudoku.csv", index=False, header=False)
